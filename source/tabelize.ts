897f43104b780981510268a84a1e89123c8adee4
Tecardo1 <tecardo@gmail.com>
2021-04-24 22:14:04 +0000

    migrated external libaries to internal functions and removed dependencies

---
+++ source/tabelize.ts
@@ -0,0 +1,32 @@
+function isObject(val) {
+  return val != null && typeof val === "object" && Array.isArray(val) === false;
+}
+
+export default function tableize(obj: any) {
+  var target = {};
+  flatten(target, obj, "");
+  return target;
+}
+
+/**
+ * Recursively flatten object keys to use dot-notation.
+ *
+ * @param {Object} `target`
+ * @param {Object} `obj`
+ * @param {String} `parent`
+ */
+
+function flatten(target: any, obj: any, parent: any) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      var val = obj[key];
+
+      key = parent + key;
+      if (isObject(val)) {
+        flatten(target, val, key + ".");
+      } else {
+        target[key] = val;
+      }
+    }
+  }
+}
