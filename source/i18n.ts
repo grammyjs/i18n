667855bbc8e9ea1690e4e2011f60873e8eca48fb
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-11-23 08:16:24 +0100

    style: adapt to linter style

--- source/i18n.ts
+++ source/i18n.ts
@@ -120,13 +120,12 @@ export class I18n {
 
 	middleware(): (ctx: MinimalMiddlewareContext, next: () => Promise<void>) => Promise<void> {
 		// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-    return async (ctx, next) => {
+		return async (ctx, next) => {
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 			const session: Session | undefined = await (this.config.useSession && (ctx as any)[this.config.sessionName]);
 			const languageCode = session?.__language_code ?? ctx.from?.language_code ?? this.config.defaultLanguage;
 
 			// @ts-expect-error writing to readonly property
-      // noinspection JSConstantReassignment
 			ctx.i18n = new I18nContext(
 				this.repository,
 				this.config,
