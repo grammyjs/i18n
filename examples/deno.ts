5993e5b2ba993eefe1f2c518c561343d8005e145
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-06-22 17:53:23 +0530

    update example
    - todo: update node example

--- examples/deno.ts
+++ examples/deno.ts
@@ -1,28 +1,28 @@
 import {
   Bot,
-  Context as BaseContext,
+  Context,
   session,
   SessionFlavor,
-} from "https://deno.land/x/grammy@v1.8.3/mod.ts";
+} from "https://deno.land/x/grammy@v1.9.0/mod.ts";
 import { I18n, I18nContextFlavor } from "../src/mod.ts";
 
 interface SessionData {
-  __locale?: string;
+  __language_code?: string;
   apples: number;
 }
 
 type MyContext =
-  & BaseContext
+  & Context
   & I18nContextFlavor
   & SessionFlavor<SessionData>;
 
 const bot = new Bot<MyContext>(""); // <-- Put your bot token here
 
-function initial(): SessionData {
-  return { apples: 0 };
-}
-
-bot.use(session({ initial }));
+bot.use(session({
+  initial: (): SessionData => {
+    return { apples: 0 };
+  },
+}));
 
 const i18n = new I18n({
   directory: "locales",
@@ -38,8 +38,8 @@ bot.command("start", async (ctx) => {
 
 bot.command("en", async (ctx) => {
   // If 'useSession' is set to true, updates the session;
-  // and if not, only uses the locale temporarily, which is
-  // equivalent for ctx.i18n.useLocale("en");
+  // otherwise throws an error. You can use ctx.i18n.useLocale("en")
+  // instead to suppress the error.
   await ctx.i18n.setLocale("en");
   await ctx.reply(ctx.t("language-set"));
 });
@@ -47,13 +47,14 @@ bot.command("en", async (ctx) => {
 // Set locale to 'ru'
 bot.command("ru", async (ctx) => {
   // This is the manual way of doing ctx.i18n.setLocale("ru");
-  ctx.session.__locale = "ru";
+  ctx.session.__language_code = "ru";
+  // To refresh the chosen locale according to
+  // the new session updates.
   await ctx.i18n.reNegotiateLocale();
 
   await ctx.reply(ctx.t("language-set"));
 });
 
-// Add apple to cart
 bot.command("add", async (ctx) => {
   ctx.session.apples++;
   await ctx.reply(ctx.t("cart", {
@@ -69,7 +70,8 @@ bot.command("cart", async (ctx) => {
 
 bot.command("checkout", async (ctx) => {
   ctx.session.apples = 0;
-  // Should log "Translation message (checkout) is not found for locale(s): ru"
+  // There is no message with the id 'checkout' in Russian translation. So, it
+  // should log "Translation message (checkout) is not found for locale(s): ru"
   // and fall back to English, if the session locale is Russian.
   await ctx.reply(ctx.t("checkout"));
 });
