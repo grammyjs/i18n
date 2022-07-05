3588773d76f4434bb10e73a7e3b8c80eac8216d6
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-05 18:47:30 +0530

    fmt

--- scripts/dnt.ts
+++ scripts/dnt.ts
@@ -48,7 +48,7 @@ await build({
     "https://lib.deno.dev/x/grammy@1.x/core/client.ts": {
       name: "grammy",
       version: "^1.9.0",
-      subPath: "out/core/client"
+      subPath: "out/core/client",
     },
     "https://deno.land/x/fluent@v0.0.0/bundle/types.ts": {
       name: "@fluent/bundle",
