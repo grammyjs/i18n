213f615cc76482515c8bf0f721287d1cfa0f39d1
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-07 15:54:28 +0530

    Apply suggestions from code review
    Co-authored-by: Roj <rojserbest@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -25,26 +25,25 @@ export interface I18nContextFlavor {
     /** Returns the current locale. */
     getLocale(): Promise<string>;
     /**
-     * Equivalent for manually setting the locale in session and calling
-     * `renegotiateLocale()`. If the `useSession` in the i18n configuration is
-     * set to true, sets the locale in session. Otherwise throws an error.
+     * Equivalent for manually setting the locale in session and calling `renegotiateLocale()`.
+     * If the `useSession` in the i18n configuration is set to true, sets the locale in session.
+     * Otherwise throws an error.
      * You can suppress the error by using `useLocale()` instead.
      * @param locale Locale ID to set in the session.
      */
     setLocale(locale: LocaleId): Promise<void>;
     /**
-     * Sets the specified locale to be used for future translations. Effect
-     * lasts only for the duration of current update and is not preserved.
-     * Could be used to change the translation locale in the middle of update
-     * processing (e.g. when user changes the language).
+     * Sets the specified locale to be used for future translations.
+     * Effect lasts only for the duration of current update and is not preserved.
+     * Could be used to change the translation locale in the middle of update processing
+     * (e.g. when user changes the language).
      * @param locale Locale ID to set.
      */
     useLocale(locale: LocaleId): void;
     /**
-     * You can manually trigger additional locale negotiation by calling this
-     * method. This could be useful if locale negotiation conditions has changed
-     * and new locale must be applied (e.g. user has changed the language and
-     * you need to display an answer in new locale).
+     * You can manually trigger additional locale negotiation by calling this method.
+     * This could be useful if locale negotiation conditions has changed and new locale must be applied
+     * (e.g. user has changed the language and you need to display an answer in new locale).
      */
     renegotiateLocale(): Promise<void>;
   };
@@ -56,8 +55,9 @@ export interface I18nContextFlavor {
 
 export interface I18nConfig<C extends Context = Context> {
   /**
-   * A locale ID to use by default. This is used when locale negotiator and
-   * session (if enabled) returns an empty result. The default value is: "_en_".
+   * A locale ID to use by default.
+   * This is used when locale negotiator and session (if enabled) returns an empty result.
+   * The default value is "_en_".
    */
   defaultLocale: LocaleId;
   /**
