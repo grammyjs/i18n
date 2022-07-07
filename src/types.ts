87faf5f684c2aece10f70174c6d11ff8af1ff6be
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-07 16:52:44 +0530

    format

--- src/types.ts
+++ src/types.ts
@@ -69,7 +69,7 @@ export interface I18nConfig<C extends Context = Context> {
   fluentOptions?: FluentOptions;
   /** Bundle options to use when adding a translation to the Fluent instance. */
   fluentBundleOptions: FluentBundleOptions;
-  /** 
+  /**
    * An optional function that determines which locale to use.
    * See [Locale Negotiation](https://github.com/grammyjs/i18n#locale-negotiation) for more details.
    */
