791399c6365899b74295f6ea85d4296d49fbc3e8
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-08-31 17:07:13 +0530

    Fix hears
    [dnt] Type checking...
    src/src/i18n.ts:213:17 - error TS4058: Return type of exported function has or is using name 'Shortcuts' from external module "/home/runner/work/i18n/i18n/out/node_modules/grammy/out/filter" but cannot be named.

--- src/i18n.ts
+++ src/i18n.ts
@@ -213,7 +213,7 @@ should either enable sessions or use `ctx.i18n.useLocale()` instead.",
 export function hears(key: string) {
   return function <C extends Context & I18nFlavor>(
     ctx: C,
-  ): ctx is Filter<C, ":text" | ":caption"> {
+  ) {
     const expected = ctx.t(key);
     return ctx.hasText(expected);
   };
