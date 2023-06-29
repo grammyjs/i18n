33c3d91936188b361e6f1ae145db3f51d432b0db
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2023-06-29 14:54:44 +0530

    chore: bump deps

--- scripts/dnt.ts
+++ scripts/dnt.ts
@@ -2,7 +2,7 @@ import {
   dirname,
   fromFileUrl,
   join,
-} from "https://deno.land/std@0.176.0/path/mod.ts";
+} from "https://deno.land/std@0.192.0/path/mod.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt@0.37.0/mod.ts";
 
 import package_ from "./package.json" assert { type: "json" };
